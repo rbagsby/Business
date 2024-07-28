68d1d18ee1db8e753ddb3f42a5fdcaaca7f5c995
Rayshaun Bagsby <rayshaun12@gmail.com>
2024-07-28 15:52:59 -0400

    FIXED WEBSITE PICS

--- scripts.js
+++ scripts.js
@@ -1,21 +1,48 @@
-// script.js
-document.addEventListener('DOMContentLoaded', () => {
-  const searchIcon = document.getElementById('search-icon');
-  const searchForm = document.getElementById('search-form');
+//Button for Vetspertise
+document.getElementById('quick-view-btn').onclick = function() {
+    document.getElementById('quick-view-popup').style.display = 'block';
+}
 
-  searchIcon.addEventListener('click', () => {
-      if (searchForm.style.display === 'block') {
-          searchForm.style.display = 'none';
-      } else {
-          searchForm.style.display = 'block';
-          searchForm.querySelector('input[type="text"]').focus();
-      }
-  });
+document.getElementById('close-btn').onclick = function() {
+    document.getElementById('quick-view-popup').style.display = 'none';
+}
 
-  // Optionally hide the form if clicking outside
-  document.addEventListener('click', (event) => {
-      if (!searchForm.contains(event.target) && !searchIcon.contains(event.target)) {
-          searchForm.style.display = 'none';
-      }
-  });
-});
+window.onclick = function(event) {
+    if (event.target == document.getElementById('quick-view-popup')) {
+        document.getElementById('quick-view-popup').style.display = 'none';
+    }
+}
+
+
+
+//Button for Vet Peeves
+document.getElementById('quick-view-btn1').onclick = function() {
+    document.getElementById('quick-view-popup1').style.display = 'block';
+}
+
+document.getElementById('close-btn1').onclick = function() {
+    document.getElementById('quick-view-popup1').style.display = 'none';
+}
+
+window.onclick = function(event) {
+    if (event.target == document.getElementById('quick-view-popup1')) {
+        document.getElementById('quick-view-popup1').style.display = 'none';
+    }
+}
+
+
+
+//Button for Vetspertise Tech edition
+document.getElementById('quick-view-btn2').onclick = function() {
+    document.getElementById('quick-view-popup2').style.display = 'block';
+}
+
+document.getElementById('close-btn2').onclick = function() {
+    document.getElementById('quick-view-popup2').style.display = 'none';
+}
+
+window.onclick = function(event) {
+    if (event.target == document.getElementById('quick-view-popup2')) {
+        document.getElementById('quick-view-popup2').style.display = 'none';
+    }
+}
