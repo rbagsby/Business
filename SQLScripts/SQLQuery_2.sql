ca00013bfeaad716baf4dd37059a88672cae84e7
Rayshaun Bagsby <rayshaun12@gmail.com>
2024-08-03 13:01:36 -0400

    cleaned up the repository

--- SQLScripts/SQLQuery_2.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO Products (Id, Name, Price, Description, Quantity, ImageUrl)
-VALUES 
-    (1, 'Vetspertise', 19.99, 'Description of Product 1', 10, '/Users/rayshaun/Documents/github/business/static/media'),
-    (2, 'Vetspertise Tech Edition', 29.99, 'Description of Product 2', 10, '/Users/rayshaun/Documents/github/business/static/media'),
-    (3, 'Pet Peeves', 39.99, 'Description of Product 3', 10, '/Users/rayshaun/Documents/github/business/static/media');
